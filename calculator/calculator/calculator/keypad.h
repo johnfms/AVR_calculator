b1516940de9b61929d06b06577ffd098c1b34167
john fawzy <68279241+johnfms@users.noreply.github.com>
2025-01-19 20:57:31 +0200

    Add files via upload

---
+++ calculator/calculator/calculator/keypad.h
@@ -0,0 +1,79 @@
+/*
+ * keypad.h
+ *
+ * Created: 10/8/2024 6:34:14 PM
+ *  Author: john
+ */ 
+
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+#include <avr/io.h>        // Include AVR input/output library for I/O operations
+#include <util/delay.h>    // Include delay utility for timing functions
+
+// Define the port, pin, and data direction register for the keypad
+#define KEYPAD_PORT     PORTA
+#define KEYPAD_PIN      PINA
+#define KEYPAD_DDR      DDRA
+
+// Define masks for rows and columns of the keypad
+#define KEYPAD_ROW_MASK 0x0F    // Mask for rows (lower 4 bits)
+#define KEYPAD_COL_MASK 0xF0    // Mask for columns (upper 4 bits)
+typedef unsigned char u8;
+
+// Define the keypad layout in a 4x4 matrix
+/*char keypad[4][4] = {
+	{'+', '-', '*', '/'},
+	{'=', 3, 6,9},
+	{'0', 2, 5,8},
+	{'c', 1, 4,7}
+};*/
+char keypad[4][4] = {
+	{'+', '-', '*', '/'},
+	{'=', '3', '6','9'},
+	{'0', '2', '5','8'},
+	{'c', '1', '4','7'}
+};
+// Function to read the pressed key on the keypad
+char readKeypad(){
+	
+	while(1){
+		// Loop through the columns
+		
+		for (u8 col = 0; col < 4; col++)
+		{
+			// Set data direction for columns and activate a single column
+			KEYPAD_DDR = KEYPAD_COL_MASK;
+			KEYPAD_PORT = ~(1 << (col + 4));  // Shift to the correct column position
+			
+			// Check each row for a pressed key
+			for (u8 row = 0; row < 4; row++)
+			{
+				// If a key is pressed in the current row
+				if (!(KEYPAD_PIN & (1 << row)))
+				{
+					// Wait for key release
+					while (!(KEYPAD_PIN & (1 << row)));
+					// Return the corresponding character from the keypad matrix
+					return keypad[row][col];
+				}
+			}
+		}
+		
+	}
+	return 0;  // This line should technically never be reached
+}
+
+// Function to scan the keypad and return the pressed key
+char keypad_scan(){
+	char key = readKeypad();  // Read the pressed key
+	if (key != 0){
+		// Display the pressed key on the LCD (assuming LCD_DISPLAY_CHAR is defined elsewhere)
+		lcd_char(key);
+		//lcd_char('O');
+	}
+	return key;  // Return the pressed key
+}
+
+
+#endif /* KEYPAD_H_ */
---
+++ calculator/calculator/calculator/main.c
@@ -0,0 +1,106 @@
+/*calculator.c Created: 13/8/2024 10:13:09 PM Author : john*/ 
+#include <avr/io.h>
+#include <string.h>
+#include "lcd.h"
+#include "keypad.h"
+#define F_CPU 8000000UL
+
+float tonum(char arr[]){
+	float num=0;
+	for (char i =0;arr[i]!='\0';i++){
+		 num =num*10+(arr[i]-'0');
+	}
+	return num;
+}
+int main(void){
+	lcd_init();
+	char num1arr[10];
+	char num2arr[10];
+	int change = 0;
+	char i=0;
+	float result=0;
+	char result_str[10];
+	char op;
+	char firsttime=1;
+	float num1=0;
+	float num2=0;
+    while (1) {	
+		char key = keypad_scan();
+		if (firsttime!=1 && (key =='+' || key=='-'  || key=='*'  || key=='/')){
+				change=1;
+				num1arr[i]='\0';
+				num1=result;
+				i=0;
+				op = key;
+		}else if ( (key =='+' || key=='-'  || key=='*'  || key=='/')){
+			if (firsttime == 1) {
+				num1arr[i] = '\0';
+				num1 = tonum(num1arr);
+				firsttime = 0;
+			}
+			change=1;
+			i=0;
+			op = key;
+		}else if(key == '='){
+			change=0;
+			num2arr[i]='\0';
+			i=0;
+			num2=tonum(num2arr);
+			switch(op) {
+				case '+':result = num1 + num2;break;
+				case '-':result = num1 - num2;break;
+				case '*':result = num1 * num2;break;
+				case '/':
+				if (num2 != 0) {
+					result = num1 / num2;
+					} else {
+					lcd_cmd(0xc0);
+					lcd_str("Error: Div by 0");
+					for (int j = 0; j < 10; j++) {
+						num1arr[j] = 0;
+						num2arr[j] = 0;
+					}
+					change = 0;
+					i = 0; 
+					_delay_ms(500);
+					lcd_cmd(0x01);
+					lcd_cmd(0x80);
+					firsttime=1;
+					continue;
+				}
+				break;
+			}
+			lcd_cmd(0xc0);
+			dtostrf(result, 0, 2, result_str);//double to string
+			lcd_cmd(0xC0);
+			lcd_str(result_str);
+			
+			for (int j = 0; j < 10; j++) {
+				num1arr[j] = 0;
+				num2arr[j] = 0;
+			}
+			change = 0;
+			i = 0;
+			_delay_ms(500);
+			lcd_cmd(0x01);
+			lcd_cmd(0x80);
+			lcd_str(result_str);
+			}else if(key=='c'){
+				lcd_cmd(0x01);
+				firsttime=1;
+				result=0;
+				num1=0;
+				num2=0;
+				change=0;
+				i=0;
+				}else{
+			if (change==0){
+				num1arr[i]=key;
+				i++;
+			}else if (change==1){
+				num2arr[i]=key;
+				i++;
+			}
+		}
+    }
+}
